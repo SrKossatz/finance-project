16d4e924775703d83bb5fef073e16e0758c01989
Kossatz <atyllakossatz@gmail.com>
2024-09-11 17:09:30 +0100

    creating frame 3

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
+# importar modulo para criar calendario
+from tkcalendar import Calendar, DateEntry
+from datetime import date
+
 
 # ################# cores ###############
 co0 = "#2e2d2b"  
@@ -27,7 +31,7 @@ co9 = "#a9a9a9"
 
 # Criando frames
 janela = Tk()
-janela.title("Fianças")
+janela.title("Finanças")
 janela.geometry("900x650")
 janela.configure(bg=co9)
 janela.resizable(False, False)
@@ -122,14 +126,14 @@ def grafico_barra():
 def resumo():
   valor = [500, 1000, 1500]
   
-  l_linha = Label(frame2, text='', width=215, height=1, anchor=NW, font=('Arial 1'))
+  l_linha = Label(frame2, text='', width=150, height=1, anchor=NW, font=('Arial 1'))
   l_linha.place(x=309, y=52)
   l_sumario = Label(frame2, text='Renda Mensal      '.upper(), anchor=NW, font=('Verdana 12'), bg=co1, fg='#000')
   l_sumario.place(x=309, y=35)
   l_sumario = Label(frame2, text='€ {:,.2f}'.format(valor[0]), anchor=NW, font=('Arial 12'), bg=co1, fg='#545454')
   l_sumario.place(x=309, y=70)
   
-  l_linha = Label(frame2, text='', width=215, height=1, anchor=NW, font=('Arial 1'))
+  l_linha = Label(frame2, text='', width=170, height=1, anchor=NW, font=('Arial 1'))
   l_linha.place(x=309, y=132)
   l_sumario = Label(frame2, text='Despesas Mensais      '.upper(), anchor=NW, font=('Verdana 12'), bg=co1, fg='#000')
   l_sumario.place(x=309, y=115)
@@ -177,8 +181,102 @@ resumo()
 grafico_pizza()
 
 
+# Criar frames dentro do frame3
+frame3_1 = Frame(frame3, width=300, height=250, bg=co1)
+frame3_1.grid(row=0, column=0)
+
+frame3_2 = Frame(frame3, width=220, height=250, bg=co1)
+frame3_2.grid(row=0, column=1, padx=5)
+
+frame3_3 = Frame(frame3, width=220, height=250, bg=co1)
+frame3_3.grid(row=0, column=2, padx=5)
 
 
+# tabela renda mensal
+
+titlo_tabela_renda = Label(frame2, text='Tabela Receitas e Despesas', anchor='nw', font=('Verdana 12 bold'), bg=co1, fg=co4)
+titlo_tabela_renda.place(x=5, y=309)
+
+# Função para mostrar a tabela de renda
+
+def mostrar_renda():
+    # creating a treeview with dual scrollbars
+    tabela_head = ['#Id','Categoria','Data','Quantia']
+
+    lista_itens = [[0,2,3,4],[0,2,3,4],[0,2,3,4],[0,2,3,4]]
+
+    global tree
+
+    tree = ttk.Treeview(frame3_1, selectmode="extended",columns=tabela_head, show="headings")
+    # vertical scrollbar
+    vsb = ttk.Scrollbar(frame3_1, orient="vertical", command=tree.yview)
+
+    # horizontal scrollbar
+    hsb = ttk.Scrollbar(frame3_1, orient="horizontal", command=tree.xview)
+
+    # Serve para rolar a tabela
+    tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+    tree.grid(column=0, row=0, sticky='nsew')
+    vsb.grid(column=1, row=0, sticky='ns')
+    hsb.grid(column=0, row=1, sticky='ew')
+
+    # Tamanhos das colunas
+    hd=["center","center","center", "center"]
+    h=[30,100,100,100]
+    n=0
+
+    for col in tabela_head:
+        tree.heading(col, text=col.title(), anchor=CENTER)
+        # ajustar a largura da coluna para a string do cabeçalho
+        tree.column(col, width=h[n],anchor=hd[n])
+
+        n+=1
+
+    for item in lista_itens:
+        tree.insert('', 'end', values=item)
+
+mostrar_renda()
+
+# Configurações de despesas
+label_info = Label(frame3_2, text='Insira novas Despesas', font=('Verdana 10 bold'), bg=co1, fg=co4, anchor='nw')
+label_info.place(x=10, y=10)
+
+# categoria
+label_categoria = Label(frame3_2, text='Categoria', font=('Ivy 10'), bg=co1, fg=co4, anchor='nw')
+label_categoria.place(x=10, y=40)
+
+lista_categoria = ['Viagem', 'Comida']
+categoria = []
+
+for i in lista_categoria:
+    categoria.append(i[1]) # Adiciona o valor da categoria
+
+combo_categoria_despesas = ttk.Combobox(frame3_2, width=10, font=('Ivy 10'))
+combo_categoria_despesas['values'] = categoria
+combo_categoria_despesas.place(x=110, y=41)
+
+
+# Despesas
+
+label_calendario_despesas = Label(frame3_2, text='Data', font=('Ivy 10'), bg=co1, fg=co4, anchor='nw')
+label_calendario_despesas.place(x=10, y=70)
+calendario_despesa = DateEntry(frame3_2, width=12, background='darkblue', foreground='white', borderwidth=2, year=2023) 
+calendario_despesa.place(x=110, y=71)
+
+# Valor
+
+label_valor_despesas = Label(frame3_2, text='Valor', font=('Ivy 10'), bg=co1, fg=co4, anchor='nw')
+label_valor_despesas.place(x=10, y=100)
+valor_despesas = Entry(frame3_2, width=12, font=('Ivy 10'), justify='left', relief='solid')
+valor_despesas.place(x=110, y=101)
+
+# Botão para adicionar despesas
+img_button = Image.open('img/add.png')
+img_button = img_button.resize((17, 17))
+img_button = ImageTk.PhotoImage(img_button)
+
+button_despesas = Button(frame3_2, image=img_button, text=' Adicionar'.upper(), width=80, compound=LEFT, anchor='nw', font=('Ivy 7 bold'), bg=co1, fg=co0, overrelief=RIDGE)
+button_despesas.place(x=110, y=131)
 
 
 
